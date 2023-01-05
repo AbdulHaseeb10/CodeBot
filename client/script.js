5b877fcf263e366f687e75f9e63147f89f34a17a
Abdul Haseeb <mohammedabdulhaseeb4@gmail.com>
2023-01-05 17:27:35 +0530

    reinstalled vite

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-import bot from "./images/bot.svg";
-import user from "./images/user.svg";
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
 
