b31b0e6c3d45ee81164ec004a062deb71b1b0d06
Abdul Haseeb <mohammedabdulhaseeb4@gmail.com>
2023-04-08 11:23:39 +0530

    redeployed the server

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from sever -> bot's response
 
-  const response = await fetch("https://codebot-88h6.onrender.com", {
+  const response = await fetch("https://codebot-67mk.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
